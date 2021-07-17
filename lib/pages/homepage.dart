cbccf5f08ba6ac41f5cba0e7ba391d2fd5218bbf
Ruchi71 <ruchichoubey.7197@gmail.com>
2021-07-17 19:15:59 +0530

    added

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,164 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:notesapp/pages/addnote.dart';
+import 'package:notesapp/pages/viewnote.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  CollectionReference ref = FirebaseFirestore.instance
+      .collection('users')
+      .doc(FirebaseAuth.instance.currentUser.uid)
+      .collection('notes');
+
+  List<Color> myColors = [
+    Colors.yellow[100],
+    Colors.red[400],
+    Colors.green[300],
+    Colors.deepPurple[500],
+    Colors.purple[500],
+    Colors.cyan[500],
+    Colors.teal[500],
+    Colors.tealAccent[100],
+    Colors.pink[500],
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context)
+              .push(
+            MaterialPageRoute(
+              builder: (context) => AddNote(),
+            ),
+          )
+              .then((value) {
+            print("Calling Set  State !");
+            setState(() {});
+          });
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white70,
+        ),
+        backgroundColor: Colors.grey[700],
+      ),
+      //
+      appBar: AppBar(
+        title: Text(
+          "Blogs",
+          style: TextStyle(
+            fontSize: 32.0,
+            fontFamily: "lato",
+            fontWeight: FontWeight.bold,
+            color: Colors.white70,
+          ),
+        ),
+        elevation: 0.0,
+        backgroundColor: Colors.orange[800],
+      ),
+      //
+      body: FutureBuilder<QuerySnapshot>(
+        future: ref.get(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            if (snapshot.data.docs.length == 0) {
+              return Center(
+                child: Column(
+                  children: [
+                Expanded(
+                child: Container(
+                decoration: BoxDecoration(
+                image: DecorationImage(
+                image: AssetImage(
+                "assets/images/bgimage.jpg",
+                ),
+              ),
+          ),
+          ),
+          ),
+              Text('You have no saved Notes!',style: TextStyle(color: Colors.purple[200]),)]
+                ),
+          );
+            }
+
+            return ListView.builder(
+              itemCount: snapshot.data.docs.length,
+              itemBuilder: (context, index) {
+                Random random = new Random();
+                Color bg = myColors[random.nextInt(4)];
+                Map data = snapshot.data.docs[index].data();
+                DateTime mydateTime = data['created'].toDate();
+                String formattedTime =
+                    DateFormat.yMMMd().add_jm().format(mydateTime);
+
+                return InkWell(
+                  onTap: () {
+                    Navigator.of(context)
+                        .push(
+                      MaterialPageRoute(
+                        builder: (context) => ViewNote(
+                          data,
+                          formattedTime,
+                          snapshot.data.docs[index].reference,
+                        ),
+                      ),
+                    )
+                        .then((value) {
+                      setState(() {});
+                    });
+                  },
+                  child: Card(
+                    color: bg,
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "${data['title']}",
+                            style: TextStyle(
+                              fontSize: 24.0,
+                              fontFamily: "lato",
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black87,
+                            ),
+                          ),
+                          //
+                          Container(
+                            alignment: Alignment.centerRight,
+                            child: Text(
+                              formattedTime,
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                fontFamily: "lato",
+                                color: Colors.black87,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          } else {
+            return Center(
+              child: Text("Loading..."),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
